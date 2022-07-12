6b9edf8a10b31ae461e8e6f60ca826af56176826
Natallia Smoliak <natallia.smoliak@external.t-systems.com>
2022-07-12 12:10:17 +0000

    [GAIAXFC-54] add and configure openAPI maven plugin

---
+++ fc-service-server/src/main/java/eu/gaiax/difs/fc/server/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package eu.gaiax.difs.fc.server.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The OpenApi Spring config.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class OpenApiConfig {
+    private final Optional<BuildProperties> buildProperties;
+
+    /**
+     * The OpenApi Info bean config.
+     */
+    @Bean
+    public OpenAPI openApiInfo() {
+        String version;
+
+        if (buildProperties.isPresent()) {
+            version = buildProperties.get().getVersion();
+        } else {
+            version = "Development Build";
+        }
+
+        return new OpenAPI().info(new Info().version(version).title("GAIA-X Catalogue")
+                .description("This is the REST API of the Gaia-X catalogue.")
+                .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
