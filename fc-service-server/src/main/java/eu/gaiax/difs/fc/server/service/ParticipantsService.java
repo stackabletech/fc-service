0b82f2a54f15b7fe8b28183fd8f2479cc3801b68
Denis Sukhoroslov <denis.sukhoroslov@t-systems.com>
2022-07-13 12:29:03 +0300

    demo-implementation for one Participants API method, just to get a common context

---
+++ fc-service-server/src/main/java/eu/gaiax/difs/fc/server/service/ParticipantsService.java
@@ -0,0 +1,24 @@
+package eu.gaiax.difs.fc.server.service;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import eu.gaiax.difs.fc.api.generated.model.Participant;
+import eu.gaiax.difs.fc.server.generated.controller.ParticipantsApiDelegate;
+
+@Service
+public class ParticipantsService implements ParticipantsApiDelegate {
+    
+    @Override
+    public ResponseEntity<Participant> addParticipant(Participant participant) {
+        // store Participant in DB, create SD for it etc.. 
+        // here we can do our own implementation with some our helper classes, may be using Data Access Layer, 
+        // or we will use components provided by Fraunhofer to work with their entities.. 
+        Optional<Participant> created = Optional.of(participant); 
+        return new ResponseEntity<>(HttpStatus.CREATED).of(created);
+    }    
+
+}
