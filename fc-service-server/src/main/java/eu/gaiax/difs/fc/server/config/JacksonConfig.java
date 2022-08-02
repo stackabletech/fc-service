a0fec567f6d7afabeba3d976b06ac2fc6e897117
SK-KOSICE\nsmoliak <Natallia.Smoliak@external.t-systems.com>
2022-08-02 15:31:00 +0300

    [GAIAXFC-84] add javadocs for roles service and configs

--- fc-service-server/src/main/java/eu/gaiax/difs/fc/server/config/JacksonConfig.java
+++ fc-service-server/src/main/java/eu/gaiax/difs/fc/server/config/JacksonConfig.java
@@ -7,8 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * Configurations for working with JSON.
+ */
 @Configuration
 public class JacksonConfig {
+  /**
+   * Provides functionality for reading and writing JSON, either to and from basic POJOs, or to and from
+   * a general-purpose JSON Tree Model (JsonNode), as well as related functionality for performing conversions.
+   *
+   * @return ObjectMapper
+   */
   @Bean
   @Primary
   public ObjectMapper objectMapper() {
