3b795fc04f469cb4046b956f941b142067887ee7
nik77612 <nikhil.acharya@iais.fraunhofer.de>
2022-08-19 00:06:40 +0200

    Query Data Interface

--- fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/graphdb/QueryGraph.java
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/graphdb/QueryGraph.java
@@ -1,25 +1,22 @@
 package eu.gaiax.difs.fc.core.service.graphdb;
 
-import eu.gaiax.difs.fc.core.pojo.OpenCypherQuery;
+import eu.gaiax.difs.fc.core.pojo.GraphQuery;
+import eu.gaiax.difs.fc.core.pojo.SdClaim;
 
 import java.util.List;
 import java.util.Map;
 
-/**
-* Defines the required functions to query the Graph DB
-* @author nacharya
-*/
 public interface QueryGraph {
 
 
     /**
-     * Query the graph with the openCypher query passed as parameter and this
+     * Query the graph when  Cypher query is passed in query object and this
      * returns list of Maps with key value pairs as a result.
      *
-     * @param query An openCypher query to be executed
-     * @return List of Maps containing the query results
+     * @param sdQuery Query to execute
+     * @return List of Maps
      */
-    List<Map<String, String>> queryData(OpenCypherQuery query);
+    public List<Map<String, Object>> queryData(GraphQuery sdQuery);
 
 
 }
