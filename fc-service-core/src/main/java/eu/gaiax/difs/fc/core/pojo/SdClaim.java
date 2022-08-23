88ae7e98cec964dfe6c77fc7184d3a6f3484ac3b
nik77612 <nikhil.acharya@iais.fraunhofer.de>
2022-08-23 10:34:15 +0200

    lombok modification

--- fc-service-core/src/main/java/eu/gaiax/difs/fc/core/pojo/SdClaim.java
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/pojo/SdClaim.java
@@ -8,8 +8,8 @@ package eu.gaiax.difs.fc.core.pojo;
 @lombok.Getter
 public class SdClaim {
 
-  private final String subject;
-  private final String predicate;
-  private final String object;
+	private final String subject;
+	private final String predicate;
+	private final String object;
 
 }
