bbff45220286801b68bf1734549ad5076fff4a9b
nik77612 <nikhil.acharya@iais.fraunhofer.de>
2022-08-23 10:43:22 +0200

    MR73 rename Graph Interface

--- fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/graphdb/GraphStore.java
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/graphdb/GraphStore.java
@@ -4,12 +4,12 @@ import eu.gaiax.difs.fc.core.pojo.SdClaim;
 
 import java.util.List;
 
-public interface SDGraphStore {
+public interface GraphStore {
 
 
     /**
      * Pushes set of claims to the Graph db. The set of claims are list of claim
-     * object containing subject, predicate and object in the form of ntriples
+     * objects containing subject, predicate and object in the form of ntriples
      * format stored in individual strings.
      *
      * @param sdClaimList List of claims to add to the Graph DB.
