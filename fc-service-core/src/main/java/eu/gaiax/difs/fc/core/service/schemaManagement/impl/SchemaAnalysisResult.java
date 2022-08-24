c757b83ee65a46bc40b103a88647421ea9412de6
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2022-08-24 13:53:26 +0000

    Feature/Schema management implementation

---
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/schemaManagement/impl/SchemaAnalysisResult.java
@@ -0,0 +1,34 @@
+package eu.gaiax.difs.fc.core.service.schemaManagement.impl;
+
+import eu.gaiax.difs.fc.core.service.schemaManagement.SchemaStore.SchemaType;
+import java.util.List;
+
+/**
+ *
+ * @author hylke
+ */
+@lombok.AllArgsConstructor
+@lombok.NoArgsConstructor
+@lombok.Getter
+@lombok.Setter
+public class SchemaAnalysisResult {
+
+  /**
+   * Flag indicating the schema is a valid schema.
+   */
+  private boolean valid;
+  /**
+   * The detected type of the schema.
+   */
+  private SchemaType schemaType;
+  /**
+   * The identifier of the schema if it can be extracted from the schema. Null
+   * otherwise.
+   */
+  private String extractedId;
+  /**
+   * The URLs of the entities that are defined in this schema.
+   */
+  private List<String> extractedUrls;
+
+}
