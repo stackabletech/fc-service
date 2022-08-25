5badda797d425fe0e43e5dd1b1f764d1d893604b
die31190 <diego.collarana.vargas@iais.fraunhofer.de>
2022-08-25 14:57:09 +0200

    We update the GraphStore interface to receive credentialSubject parameter as the self-description unique identifier.

--- fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/graphdb/GraphStore.java
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/service/graphdb/GraphStore.java
@@ -12,13 +12,20 @@ public interface GraphStore {
 
 
     /**
-     * Pushes set of claims to the Graph db. The set of claims are list of claim
+     * Pushes set of claims to the Graph DB. The set of claims are list of claim
      * objects containing subject, predicate and object similar to the form of n-triples
      * format stored in individual strings.
      *
      * @param sdClaimList List of claims to be added to the Graph DB.
+     * @param credentialSubject contains a self-description unique identifier
      */
-    void addClaims(List<SdClaim> sdClaimList);
+    void addClaims(List<SdClaim> sdClaimList, String credentialSubject);
+
+    /**
+     * Deletes all claims in the Graph DB of a given self-description
+     * @param credentialSubject contains a self-description unique identifier
+     */
+    void deleteClaims(String credentialSubject);
 
 }
 
