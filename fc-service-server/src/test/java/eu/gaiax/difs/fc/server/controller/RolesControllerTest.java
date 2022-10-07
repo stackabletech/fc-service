4474309af93db3ec5b3b75543fb2ea38e71b5f1c
Natallia Smoliak <natallia.smoliak@external.t-systems.com>
2022-10-07 10:04:42 +0000

    rollback embedded keycloak test to mocks

--- fc-service-server/src/test/java/eu/gaiax/difs/fc/server/controller/RolesControllerTest.java
+++ fc-service-server/src/test/java/eu/gaiax/difs/fc/server/controller/RolesControllerTest.java
@@ -2,19 +2,28 @@ package eu.gaiax.difs.fc.server.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import eu.gaiax.difs.fc.server.controller.common.EmbeddedKeycloakTest;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RolesResource;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,18 +36,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider;
 
+@SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 @AutoConfigureEmbeddedDatabase(provider = DatabaseProvider.ZONKY)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {"server.port=9093"})
-public class RolesControllerTest extends EmbeddedKeycloakTest {
+public class RolesControllerTest {
 
-    private static final TypeReference<List<?>> LIST_TYPE_REF = new TypeReference<>() {
+    private static final TypeReference<List<?>> LIST_TYPE_REF = new TypeReference<List<?>>() {
     };
 
     @Autowired
     private WebApplicationContext context;
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private KeycloakBuilder builder;
+    @MockBean
+    private Keycloak keycloak;
+    @MockBean
+    private RealmResource realmResource;
+    @MockBean
+    private RolesResource rolesResource;
     @Autowired
     private  ObjectMapper objectMapper;
 
@@ -49,15 +69,15 @@ public class RolesControllerTest extends EmbeddedKeycloakTest {
 
     @Test
     public void getRolesShouldReturnUnauthorizedResponse() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/roles")
-            .contentType(MediaType.APPLICATION_JSON)
-)
-            .andExpect(status().isUnauthorized());
+        mockMvc.perform(MockMvcRequestBuilders.post("/roles")).andExpect(status().isUnauthorized());
     }
 
     @Test
     @WithMockUser
     public void getRolesShouldReturnExpectedNumber() throws Exception {
+
+        setupKeycloak();
+
         MvcResult result = mockMvc
             .perform(MockMvcRequestBuilders.get("/roles")
             .contentType(MediaType.APPLICATION_JSON))
@@ -65,7 +85,16 @@ public class RolesControllerTest extends EmbeddedKeycloakTest {
             .andReturn();
         List<?> roles = objectMapper.readValue(result.getResponse().getContentAsString(), LIST_TYPE_REF);
         assertNotNull(roles);
-        // default roles + app roles added from the gaia-x-realms file import
-        assertEquals(7, roles.size());
+        assertEquals(3, roles.size());
     }
+
+    private void setupKeycloak() {
+        when(builder.build()).thenReturn(keycloak);
+        when(keycloak.realm("gaia-x")).thenReturn(realmResource);
+        when(realmResource.roles()).thenReturn(rolesResource);
+        List<RoleRepresentation> roles = List.of(new RoleRepresentation("RO_MA_A", null, false),
+                new RoleRepresentation("RO_MA_B", null, true), new RoleRepresentation("RO_MA_C", null, false));
+        when(rolesResource.list(true)).thenReturn(roles);
+    }
+
 }
