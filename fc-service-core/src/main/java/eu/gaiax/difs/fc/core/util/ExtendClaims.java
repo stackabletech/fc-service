a0610e056d18df9a045daacece378b2ea2525ace
nik77612 <nikhil.acharya@iais.fraunhofer.de>
2022-12-07 18:00:54 +0100

    return multiproperties

--- fc-service-core/src/main/java/eu/gaiax/difs/fc/core/util/ExtendClaims.java
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/util/ExtendClaims.java
@@ -7,6 +7,7 @@ import org.apache.jena.vocabulary.RDF;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ExtendClaims {
@@ -19,6 +20,8 @@ public class ExtendClaims {
      * @param credentialSubject
      * @return Triples as string
      */
+
+
     public static String addPropertyGraphUri(Model claims, String credentialSubject) {
         Literal credentialSubjectLiteral = ResourceFactory.createStringLiteral(credentialSubject);
         Property claimsGraphUri = ResourceFactory.createProperty("http://w3id.org/gaia-x/service#claimsGraphUri");
@@ -31,7 +34,6 @@ public class ExtendClaims {
             Resource s = triple.getSubject();
             Property p = triple.getPredicate();
             RDFNode o = triple.getObject();
-
             additionalTriples.add(
                     new StatementImpl(
                             s,
@@ -55,7 +57,28 @@ public class ExtendClaims {
         claims.add(additionalTriples);
         OutputStream outputstream = new ByteArrayOutputStream();
         claims.write(outputstream, "N-TRIPLES");
-
         return outputstream.toString();
+    }
+
+
+    public static HashSet<String> getMultivalProp(Model claims) {
+        ExtendClaims extendClaims = new ExtendClaims();
+        HashSet<String> multiprop = new HashSet<String>();
+        StmtIterator triples = claims.listStatements();
+        while (triples.hasNext()) {
+            Statement triple = triples.next();
+            Resource s = triple.getSubject();
+            Property p = triple.getPredicate();
+            if (extendClaims.checkMultivalueProp(s, p))
+                multiprop.add(p.toString());
+        }
+        return multiprop;
+    }
+
+
+    private boolean checkMultivalueProp(Resource subject, Property predicate) {
+        if (subject.listProperties(predicate).toList().size() > 1)
+            return true;
+        return false;
     }
 }
